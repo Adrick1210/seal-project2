c0f3261c2a4b638aa9552b2e18cbee8e8a7c3eb4
Christian Alexander <christian.alexanderato@gmail.com>
2024-01-10 18:36:25 -0800

    Schema setup

---
+++ models/todo.js
@@ -0,0 +1,18 @@
+// DEPENDENCIES
+const mongoose = require("mongoose");
+
+// DEFINE MODEL
+const { Schema, model } = mongoose;
+
+// SCHEMA
+const todoSchema = new Schema({
+  title: String,
+  description: String,
+  isComplete: Boolean,
+});
+
+// MODEL
+const Todo = model("Todo", todoSchema);
+
+// EXPORT
+module.exports = Todo;
