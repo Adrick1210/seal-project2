5e741cb32afe782158e75aca63069a174c179abb
Christian Alexander <christian.alexanderato@gmail.com>
2024-01-17 17:11:48 -0800

    edit modal now working - styles

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// Edit Modal Functions 
+function showModal(id) {
+    // console.log(id);
+    const dialog = document.getElementById(id);
+    dialog.showModal();
+  }
+  function closeModal(id) {
+    const dialog = document.getElementById(id);
+    dialog.close();
+  }
