70b4ff27d3d9f8831ff331f1e29d6d03f27a60a1
Christian Alexander <christian.alexanderato@gmail.com>
2024-01-17 19:01:36 -0800

    show page styled - trying to work on bonus - update user pages

--- public/app.js
+++ public/app.js
@@ -1,10 +1,18 @@
-// Edit Modal Functions 
+// Edit Modal Functions
 function showModal(id) {
-    // console.log(id);
-    const dialog = document.getElementById(id);
-    dialog.showModal();
-  }
-  function closeModal(id) {
-    const dialog = document.getElementById(id);
-    dialog.close();
-  }
+  // console.log(id);
+  const dialog = document.getElementById(id);
+  dialog.showModal();
+}
+
+// Show Modal Function
+function displayModal(id) {
+  // console.log(id);
+  const dialog = document.getElementById(id);
+  dialog.showModal();
+}
+// Close Button Function
+function closeModal(id) {
+  const dialog = document.getElementById(id);
+  dialog.close();
+}
